9f776b66e24cda5154a083b66094cc4b4d468240
Abdullah Shafqat <abdullahshafqat27@hotmail.com>
2026-03-29 23:44:21 +0300

    feat: full screen mobile rendering

--- src/components/AnalyticsPanel.tsx
+++ src/components/AnalyticsPanel.tsx
@@ -8,7 +8,7 @@ import BarkBeetleRisk from "./BarkBeetleRisk";
 
 export default function AnalyticsPanel() {
   return (
-    <div className="flex flex-col gap-3 overflow-y-auto p-4 h-full">
+    <div className="flex flex-col gap-3 p-4">
       <SummaryCards />
       <TimeSeriesChart />
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-3">
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -20,13 +20,13 @@ const ForestMap = dynamic(() => import("./ForestMap"), {
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col h-screen bg-forest-bg text-forest-header">
+    <div className="flex flex-col h-dvh bg-forest-bg text-forest-header">
       <Header />
       <div className="flex flex-1 overflow-hidden flex-col lg:flex-row">
-        <div className="w-full lg:w-[60%] h-[50vh] lg:h-auto border-b lg:border-b-0 lg:border-r border-forest-border">
+        <div className="relative z-10 w-full lg:w-[60%] h-[35vh] lg:h-auto shrink-0 border-b lg:border-b-0 lg:border-r border-forest-border">
           <ForestMap />
         </div>
-        <div className="w-full lg:w-[40%] overflow-y-auto">
+        <div className="w-full lg:w-[40%] flex-1 min-h-0 overflow-y-auto">
           <AnalyticsPanel />
         </div>
       </div>
